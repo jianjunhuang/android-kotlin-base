a21147d576518cd7db9adcb059e34137578325df
JianjunHuang <demon903732218@gmail.com>
2020-09-30 17:58:24 +0800

    add viewModel creater

--- src/main/java/com/jianjun/base/compontent/BaseActivity.kt
+++ src/main/java/com/jianjun/base/compontent/BaseActivity.kt
@@ -1,6 +1,12 @@
 package com.jianjun.base.compontent
 
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
 
-open class BaseActivity : AppCompatActivity() {
+abstract class BaseActivity : AppCompatActivity() {
+
+    private inline fun <reified T : ViewModel> AppCompatActivity.viewModels() = lazy {
+        ViewModelProvider(this).get(T::class.java)
+    }
 }
--- src/main/java/com/jianjun/base/compontent/BaseFragment.kt
+++ src/main/java/com/jianjun/base/compontent/BaseFragment.kt
@@ -1,6 +1,12 @@
 package com.jianjun.base.compontent
 
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
 
-open class BaseFragment : Fragment() {
+abstract class BaseFragment : Fragment() {
+
+    private inline fun <reified T : ViewModel> Fragment.viewModels() = lazy {
+        ViewModelProvider(requireActivity()).get(T::class.java)
+    }
 }
