b2b0c88e052e60464d2ed624443f1e813895d80a
jun <demon903732218@gmail.com>
2021-09-22 22:51:59 +0800

    add click linstener

--- src/main/java/com/jianjun/base/adapter/BaseAdapter.kt
+++ src/main/java/com/jianjun/base/adapter/BaseAdapter.kt
@@ -1,16 +1,18 @@
 package com.jianjun.base.adapter
 
+import android.view.ViewGroup
 import androidx.recyclerview.widget.AsyncListDiffer
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
 
-abstract class BaseAdapter<D : Any, V : BaseViewHolder<*>> : RecyclerView.Adapter<V>() {
+abstract class BaseAdapter<D : Any, V : BaseViewHolder<D>> : RecyclerView.Adapter<V>() {
 
     protected abstract fun onDiffCallback(): DiffUtil.ItemCallback<D>
 
     private val diffUtil = AsyncListDiffer<D>(this, onDiffCallback())
 
-    protected var clickListener: OnItemClickListener? = null
+    protected var clickListener: OnItemClickListener<D>? = null
+    protected var longClickListener: OnItemLongClickListener<D>? = null
 
     override fun onBindViewHolder(holder: V, position: Int) {
         onBindViewHolder(holder, position, getData(position))
@@ -34,11 +36,40 @@ abstract class BaseAdapter<D : Any, V : BaseViewHolder<*>> : RecyclerView.Adapte
         diffUtil.submitList(dataList, callback)
     }
 
-    fun setOnItemClickListener(clickListener: OnItemClickListener) {
+    fun setOnItemClickListener(clickListener: OnItemClickListener<D>) {
         this.clickListener = clickListener
     }
 
-    interface OnItemClickListener {
-        fun onItemClicked(position: Int)
+    fun setOnItemLongClickListener(clickListener: OnItemLongClickListener<D>) {
+        this.longClickListener = clickListener
+    }
+
+    interface OnItemClickListener<D : Any> {
+        fun onItemClicked(position: Int, data: D, adapter: BaseAdapter<D, *>)
+    }
+
+    interface OnItemLongClickListener<D : Any> {
+        fun onItemLongClicked(position: Int, data: D, adapter: BaseAdapter<D, *>)
+    }
+
+    fun BaseViewHolder<D>.bindClick() {
+        this.itemView.setOnClickListener {
+            clickListener?.onItemClicked(
+                this.adapterPosition,
+                diffUtil.currentList[adapterPosition],
+                this@BaseAdapter
+            )
+        }
+    }
+
+    fun BaseViewHolder<D>.bindLongClick() {
+        this.itemView.setOnLongClickListener {
+            longClickListener?.onItemLongClicked(
+                this.adapterPosition,
+                diffUtil.currentList[adapterPosition],
+                this@BaseAdapter
+            )
+            return@setOnLongClickListener true
+        }
     }
 }
