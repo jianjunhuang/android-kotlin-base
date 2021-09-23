7247d0560fb190a5dab3a2636318ebe5a3f7cb28
jun <demon903732218@gmail.com>
2021-09-23 13:50:33 +0800

    add keyboard utils

---
+++ src/main/java/com/jianjun/base/utils/KeyboardUtils.kt
@@ -0,0 +1,28 @@
+package com.jianjun.base.utils
+
+import android.app.Activity
+import android.graphics.Rect
+
+object KeyboardUtils {
+    fun Activity.observeKeyboardChange(onChange: (isShowing: Boolean) -> Unit) {
+        val rootView = this.window.decorView
+        val r = Rect()
+        var lastHeight = 0
+        rootView.viewTreeObserver.addOnGlobalLayoutListener {
+            rootView.getWindowVisibleDisplayFrame(r)
+            val height = r.height()
+            if (lastHeight == 0) {
+                lastHeight = height
+            } else {
+                val diff = lastHeight - height
+                if (diff > 200) {
+                    onChange(true)
+                    lastHeight = height
+                } else if (diff < -200) {
+                    onChange(false)
+                    lastHeight = height
+                }
+            }
+        }
+    }
+}
