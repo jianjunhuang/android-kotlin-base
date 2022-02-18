61663e6d8e58473aaab1f7fe23b0487c32908d01
jun <demon903732218@gmail.com>
2022-02-18 11:40:40 +0800

    feature: color.colorWithAlpha, add alpha to color

--- src/main/java/com/jianjun/base/ext/ColorExt.kt
+++ src/main/java/com/jianjun/base/ext/ColorExt.kt
@@ -14,3 +14,12 @@ val Int.toHexColor
 
 val Int.toHexColorAlpha
     get() = String.format("#%08X", 0xFFFFFFFF and this.toLong())
+
+/**
+ * change the alpha with color
+ * @param alpha -> [0f, 1f]
+ */
+inline fun Int.colorWithAlpha(alpha: Float): Int {
+    if (alpha !in 0f..1f) throw IllegalArgumentException("alpha must in [0f, 1f]")
+    return (this and 0x00FFFFFF) or ((alpha * 255).toInt() shl 24)
+}
